7870e8fc573c1566cf93c5670c40020c16316b52
Alex Windows <13nicholasa@gmail.com>
2023-09-21 22:33:14 +0100

    intial commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
 import EditPost from "./pages/EditPost";
 
+//List of pages
 function App() {
   return (
     <UserContextProvider>
